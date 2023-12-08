b686e287e6a460693ae50d5d897722fc22bb8ed8
Artur Czopek <artur.czopek@dac.digital>
2023-12-08 10:39:16 +0100

    Day 08 - part B / refactor

--- src/main/kotlin/pl/simplecoding/aoc/Day08.kt
+++ src/main/kotlin/pl/simplecoding/aoc/Day08.kt
@@ -1,18 +1,34 @@
 package pl.simplecoding.aoc
 
-const val LAST_STEP = "ZZZ"
+import kotlin.math.abs
+
+const val END_STEP = "ZZZ"
 const val START_STEP = "AAA"
+
+const val START_STEP_SUFFIX = 'A'
+const val END_STEP_SUFFIX = 'Z'
+
 const val LEFT_ACTION = 'L'
 
 fun main() {
     Day08a.solve(readInput("Day08")).println()
-//    Day08b.solve(readInput("Day08")).println()
+    Day08b.solve(readInput("Day08")).println()
 }
 
 abstract class Day08 {
-    abstract fun solve(input: List<String>): Long
+    fun solve(input: List<String>) = parseGuide(input).let { guide ->
+        guide.steps
+            .filter(startStepCondition)
+            .map { it.value }
+            .map { getStepsAmount(guide, it, endStepCondition) }
+            .let(finalModifier)
+    }
 
-    fun parseGuide(input: List<String>): Guide {
+    abstract val startStepCondition: (Map.Entry<String, Step>) -> Boolean
+    abstract val endStepCondition: (Step) -> Boolean
+    abstract val finalModifier: Iterable<Long>.() -> Long
+
+    private fun parseGuide(input: List<String>): Guide {
         val lines = input.map { it.trim() }
         val instruction = lines[0]
         val steps = mutableMapOf<String, Step>()
@@ -26,23 +42,57 @@ abstract class Day08 {
 
         return Guide(instruction, steps)
     }
-}
 
-
-object Day08a : Day08() {
-    override fun solve(input: List<String>): Long {
-        val guide = parseGuide(input)
-        var currentStep = guide.steps[START_STEP]!!
+    private fun getStepsAmount(guide: Guide, startStep: Step, endCondition: (Step) -> Boolean): Long {
+        var currentStep = startStep
         var stepsDone = 0L
         val stepsInGuide = guide.instructions.length
         do {
             val nextAction = guide.instructions[(stepsDone % stepsInGuide).toInt()]
-            currentStep = if (nextAction == LEFT_ACTION) guide.steps[currentStep.leftId]!! else guide.steps[currentStep.rightId]!!
+            currentStep =
+                if (nextAction == LEFT_ACTION) guide.steps[currentStep.left]!! else guide.steps[currentStep.right]!!
             stepsDone++
-        } while (currentStep.id != LAST_STEP)
+        } while (!endCondition.invoke(currentStep))
         return stepsDone
     }
+}
 
+object Day08a : Day08() {
+    override val startStepCondition: (Map.Entry<String, Step>) -> Boolean
+        get() = { it.value.id == START_STEP }
+    override val endStepCondition: (Step) -> Boolean
+        get() = { it.id == END_STEP }
+
+    override val finalModifier: Iterable<Long>.() -> Long
+        get() = Iterable<Long>::sum
+}
+
+object Day08b : Day08() {
+    override val startStepCondition: (Map.Entry<String, Step>) -> Boolean
+        get() = { it.value.id.endsWith(START_STEP_SUFFIX) }
+    override val endStepCondition: (Step) -> Boolean
+        get() = { it.id.endsWith(END_STEP_SUFFIX) }
+    override val finalModifier: Iterable<Long>.() -> Long
+        get() = Iterable<Long>::findLcm
+}
+
+fun gcd(a: Long, b: Long): Long {
+    return if (b == 0L) abs(a) else gcd(b, a % b)
+}
+
+fun lcm(a: Long, b: Long): Long {
+    return if (a == 0L || b == 0L) 0 else abs(a * b) / gcd(a, b)
+}
+
+private fun Iterable<Long>.findLcm(): Long {
+    val asList = this.toList()
+    var result = asList[0]
+
+    for (i in 1..<asList.size) {
+        result = lcm(result, asList[i])
+    }
+
+    return result
 }
 
 data class Guide(
@@ -50,8 +100,8 @@ data class Guide(
     val steps: Map<String, Step>
 )
 
-class Step(
+data class Step(
     val id: String,
-    val leftId: String,
-    val rightId: String
+    val left: String,
+    val right: String
 )
--- src/test/kotlin/pl/simplecoding/aoc/Day08Test.kt
+++ src/test/kotlin/pl/simplecoding/aoc/Day08Test.kt
@@ -25,16 +25,28 @@ class Day08Test {
             BBB = (AAA, ZZZ)
             ZZZ = (ZZZ, ZZZ)
         """.trimIndent()
+        val input3 = """
+            LR
+
+            11A = (11B, XXX)
+            11B = (XXX, 11Z)
+            11Z = (11B, XXX)
+            22A = (22B, XXX)
+            22B = (22C, 22C)
+            22C = (22Z, 22Z)
+            22Z = (22B, 22B)
+            XXX = (XXX, XXX)
+        """.trimIndent()
 
 
         // when
         val resultA1 = Day08a.solve(input1.lines())
         val resultA2 = Day08a.solve(input2.lines())
-//        val resultB = Day08b.solve(input.lines())
+        val resultB = Day08b.solve(input3.lines())
 
         // then
         assert(2L == resultA1)
         assert(6L == resultA2)
-//        assert(5905L == resultB)
+        assert(6L == resultB)
     }
 }
